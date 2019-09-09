41701f9ace772e72804bf71641756dbf628a0afb
Logan Miner <loganrobertminer@gmail.com>
2019-09-08 20:25:41 -0400

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 2000;
+
+const app = express();
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+app.use(express.static('./public'))
+
+//routes
+require('./routes/html-routes')(app);
+// require('./routes/api-routes');
+
+
+app.listen(PORT, function () {
+    console.log('listening on port' + PORT);
+    console.log('http://localhost:' + PORT);
+});
